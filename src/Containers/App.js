8eedc56049ba0f0b0db8b21f24aa62fcf38acff6
unknown <vinayakjaiswal48@gmail.com>
2020-03-21 21:08:13 +0530

    adding robofriends

---
+++ src/Containers/App.js
@@ -0,0 +1,46 @@
+import React, { Component } from "react";
+import CardList from "../Components/CardList";
+import Scroll from "../Components/Scroll";
+import SearchBox from "../Components/SearchBox";
+import ErrorBoundry from "../Components/ErrorBoundry";
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      robots: [],
+      searchfield: ""
+    };
+  }
+  componentDidMount() {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then(response => response.json())
+      .then(users => this.setState({ robots: users }));
+  }
+  onSearchChange = event => {
+    this.setState({ searchfield: event.target.value });
+  };
+  render() {
+    const filteredRobots = this.state.robots.filter(robot => {
+      return robot.name
+        .toLowerCase()
+        .includes(this.state.searchfield.toLowerCase());
+    });
+    if (this.state.robots.length === 0) {
+      return <h1>Loading</h1>;
+    } else {
+      return (
+        <div className="tc">
+          <h1>RoboFriends</h1>
+          <SearchBox searchChange={this.onSearchChange} />
+          <Scroll>
+            <ErrorBoundry>
+              <CardList robots={filteredRobots} />
+            </ErrorBoundry>
+          </Scroll>
+        </div>
+      );
+    }
+  }
+}
+export default App;
